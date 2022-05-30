305d1fafe5371ed7539a243a412af02b3c232ad7
minh1107 <minhdepzaiahihi@gmail.com>
2022-05-30 09:15:24 +0700

    add classes file

---
+++ n11_classes.js
@@ -0,0 +1,9 @@
+class ClassName {
+    constructor(year, month, day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    };
+}
+
+const date = new ClassName('2022', '5', '30');
